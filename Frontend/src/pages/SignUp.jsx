b1768a498b820bc3d382984c1c0efc746b1838dd
Sudhirpratap1 <sisodiyajivijaypratap@gmail.com>
2026-03-27 17:53:31 +0530

    Update frontend to use Render backend with Axios instance

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import api from "../Api";
 
 export default function SignUp() {
   const navigate = useNavigate();
